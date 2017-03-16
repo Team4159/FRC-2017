7d082d129a314d890163b1ab4e381fa60943a504
Cole Scott <colescottsf@gmail.com>
2017-03-15 22:00:17 -0700

    Add option for xbox controll through define in OI

--- src/OI.cpp
+++ src/OI.cpp
@@ -4,24 +4,44 @@
 
 OI::OI()
 {
+#ifndef OI_XBOX_PRIMARY
     leftStick = std::make_unique<frc::Joystick> ( 0 );
     rightStick = std::make_unique<frc::Joystick> ( 1 );
+#else
+    primaryController = std::make_unique<frc::XboxController> ( 0 );
+#endif
     secondaryStick = std::make_unique<frc::Joystick> ( 2 );
 }
 
 double OI::GetLeftJoystick()
 {
+#ifndef OI_XBOX_PRIMARY
     return leftStick->GetY();
+#else
+    return primaryController->GetY ( frc::GenericHID::JoystickHand::kLeftHand );
+#endif
 }
 
 double OI::GetRightJoystick()
 {
+#ifndef OI_XBOX_PRIMARY
     return rightStick->GetY();
+#else
+    return primaryController->GetY ( frc::GenericHID::JoystickHand::kRightHand );
+#endif
 }
 
 double OI::GetIntake()
 {
     return secondaryStick->GetY();
+}
+
+double OI::GetClimber()
+{
+    if ( !secondaryStick->GetRawButton ( 6 ) ) {
+        return 0;
+    }
+    return secondaryStick->GetZ();
 }
 
 bool OI::GetLifter()
@@ -38,13 +58,21 @@ bool OI::GetLifter()
 
 bool OI::GetShift()
 {
+#ifndef OI_XBOX_PRIMARY
     if ( rightStick->GetRawButton ( 3 ) ) {
         shifterPosition = true;
     }
     if ( rightStick->GetRawButton ( 2 ) ) {
         shifterPosition = false;
     }
-
+#else
+    if ( primaryController->GetBumper ( frc::GenericHID::JoystickHand::kLeftHand ) ) {
+        shifterPosition = true;
+    }
+    if ( primaryController->GetBumper ( frc::GenericHID::JoystickHand::kRightHand ) ) {
+        shifterPosition = false;
+    }
+#endif
     return shifterPosition;
 }
 
