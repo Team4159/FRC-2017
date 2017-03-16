af98499038e19a0b0957aec939ebee093b41d6f8
Cole Scott <colescottsf@gmail.com>
2017-03-15 22:04:21 -0700

    Add RobotMap constants

--- src/RobotMap.h
+++ src/RobotMap.h
@@ -4,6 +4,7 @@
 // Motor ports
 const int MOTOR_LEFT_DRIVE = 0;
 const int MOTOR_RIGHT_DRIVE = 1;
+const int MOTOR_CLIMBER = 2;
 const int MOTOR_INTAKE = 3;
 
 // Solenoid ports for shifting
@@ -19,7 +20,13 @@ const int SOLENOID_LIFTER_RIGHT_A = 6;
 const int SOLENOID_LIFTER_RIGHT_B = 7;
 
 // Solenoid ports for gripper
-const int SOLENOID_GRIPPER_A = 8;
-const int SOLENOID_GRIPPER_B = 9;
+const int SOLENOID_GRIPPER_A = 0;
+const int SOLENOID_GRIPPER_B = 1;
+
+// Encoder port for drivetrain
+const int ENCODER_DRIVE_LEFT_A = 0;
+const int ENCODER_DRIVE_LEFT_B = 1;
+const int ENCODER_DRIVE_RIGHT_A = 2;
+const int ENCODER_DRIVE_RIGHT_B = 3;
 
 #endif  // ROBOTMAP_H
