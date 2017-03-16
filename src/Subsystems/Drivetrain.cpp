e9a7a643e282ae21e08075565a82bd17e41ad069
Cole Scott <colescottsf@gmail.com>
2017-03-15 22:16:27 -0700

    Overhaul Drivetrain subsystem
    - Add PID for auto/image rec
    - Add current/voltage limiting logic to avoid brownouts
    - Add more cardinaldash diagnostics

--- src/Subsystems/Drivetrain.cpp
+++ src/Subsystems/Drivetrain.cpp
@@ -7,6 +7,8 @@
 
 #include <DoubleSolenoid.h>
 
+#include <cmath>
+
 std::unique_ptr<PowerDistributionPanel> Drivetrain::pdp = std::make_unique<PowerDistributionPanel>();
 
 Drivetrain::Drivetrain() : Subsystem ( "Drivetrain" )
@@ -17,8 +19,25 @@ Drivetrain::Drivetrain() : Subsystem ( "Drivetrain" )
     LeftSolenoid = std::make_unique<frc::DoubleSolenoid> ( SOLENOID_SHIFTER_LEFT_A, SOLENOID_SHIFTER_LEFT_B );
     RightSolenoid = std::make_unique<frc::DoubleSolenoid> ( SOLENOID_SHIFTER_RIGHT_A, SOLENOID_SHIFTER_RIGHT_B );
 
-    CardinalDash::Dashboard::Subscribe(std::string("subsystems/drivetrain/leftvoltage"), &GetLeftVoltage, this);
-    CardinalDash::Dashboard::Subscribe(std::string("subsystems/drivetrain/rightvoltage"), &GetRightVoltage, this);
+    LeftEncoder = std::make_unique<frc::Encoder> ( ENCODER_DRIVE_LEFT_A, ENCODER_DRIVE_LEFT_B );
+    RightEncoder = std::make_unique<frc::Encoder> ( ENCODER_DRIVE_RIGHT_A, ENCODER_DRIVE_RIGHT_B );
+
+    LeftEncoder->SetReverseDirection ( false );
+    RightEncoder->SetReverseDirection ( true );
+
+    LeftEncoder->SetDistancePerPulse ( ENCODER_DISTANCE_PER_PULSE );
+    RightEncoder->SetDistancePerPulse ( ENCODER_DISTANCE_PER_PULSE );
+
+    LeftPID = std::make_unique<frc::PIDController> ( PID_LEFT_P, PID_LEFT_I, PID_LEFT_D, PID_LEFT_F, LeftEncoder.get(), LeftVictor.get() );
+    RightPID = std::make_unique<frc::PIDController> ( PID_RIGHT_P, PID_RIGHT_I, PID_RIGHT_D, PID_RIGHT_F, RightEncoder.get(), RightVictor.get() );
+
+    LeftPID->SetContinuous ( false );
+    RightPID->SetContinuous ( false );
+
+    CardinalDash::Dashboard::Subscribe ( std::string ( "subsystems/drivetrain/leftvoltage" ), &GetLeftVoltage, this );
+    CardinalDash::Dashboard::Subscribe ( std::string ( "subsystems/drivetrain/rightvoltage" ), &GetRightVoltage, this );
+    CardinalDash::Dashboard::Subscribe ( std::string ( "subsystems/drivetrain/leftEncoder" ), &GetEncoderValue, LeftEncoder.get() );
+    CardinalDash::Dashboard::Subscribe ( std::string ( "subsystems/drivetrain/rightEncoder" ), &GetEncoderValue, RightEncoder.get() );
 }
 
 void Drivetrain::InitDefaultCommand()
@@ -26,10 +45,116 @@ void Drivetrain::InitDefaultCommand()
     SetDefaultCommand ( new TeleopDrive() );
 }
 
+void Drivetrain::SetRaw ( double left, double right )
+{
+    SetLeftRaw ( left );
+    SetRightRaw ( right );
+}
+
 void Drivetrain::Set ( double left, double right )
 {
-    DrivetrainVoltage v = DrivetrainVoltage ( left * VOLTAGE_MAX, right * VOLTAGE_MAX );
-    SetVoltage ( v );
+    LeftPID->SetSetpoint ( left );
+    RightPID->SetSetpoint ( right );
+}
+
+void Drivetrain::SetAdd ( double left, double right )
+{
+    LeftPID->SetSetpoint ( LeftPID->GetSetpoint() + left );
+    RightPID->SetSetpoint ( RightPID->GetSetpoint() + right );
+}
+
+void Drivetrain::SetLeftRaw ( double left )
+{
+    double voltage = pdp->GetVoltage();
+    double maxDiff = MAX_ACCEL * voltage;
+
+    if ( fabs ( left ) - fabs ( lastLeft ) > maxDiff ) {
+        if ( lastLeft > left ) {
+            if ( lastLeft < 0 && lastLeftGhost > 0 ) {
+                left = lastLeftGhost - maxDiff;
+            } else {
+                left = lastLeft - maxDiff;
+            }
+        } else {
+            if ( lastLeft > 0 && lastLeftGhost < 0 ) {
+                left = lastLeftGhost + maxDiff;
+            } else {
+                left = lastLeft + maxDiff;
+            }
+        }
+    }
+    if ( lastLeftGhost > lastLeft ) {
+        lastLeftGhost -= MAX_GHOST_ACCEL;
+        if ( lastLeftGhost > lastLeft ) {
+            lastLeftGhost = lastLeft;
+        }
+    } else {
+        lastLeftGhost += MAX_GHOST_ACCEL;
+        if ( lastLeftGhost < lastLeft ) {
+            lastLeftGhost = lastLeft;
+        }
+    }
+
+    double leftTotal = pdp->GetCurrent ( 0 ) + pdp->GetCurrent ( 1 ) + pdp->GetCurrent ( 2 );
+
+    if ( leftTotal > 100 ) {
+        left /= ( 100/leftTotal );
+    }
+
+    if ( voltage < 10 ) {
+        left /= ( 10/voltage );
+    }
+
+    LeftVictor->Set ( left );
+
+    lastLeft = left;
+}
+
+void Drivetrain::SetRightRaw ( double right )
+{
+    double voltage = pdp->GetVoltage();
+    double maxDiff = MAX_ACCEL * voltage;
+
+    if ( fabs ( right )- fabs ( lastRight ) > maxDiff ) {
+        if ( lastRight > right ) {
+            if ( lastRight < 0 && lastRightGhost > 0 ) {
+                right = lastRightGhost - maxDiff;
+            } else {
+                right = lastRight - maxDiff;
+            }
+        } else {
+            if ( lastRight > 0 && lastRightGhost < 0 ) {
+                right = lastRightGhost + maxDiff;
+            } else {
+                right = lastRight + maxDiff;
+            }
+        }
+    }
+    if ( lastRightGhost > lastRight ) {
+        lastRightGhost -= MAX_GHOST_ACCEL;
+        if ( lastRightGhost > lastRight ) {
+            lastRightGhost = lastRight;
+        }
+    } else if ( lastRightGhost < lastRight ) {
+        lastRightGhost += MAX_GHOST_ACCEL;
+        if ( lastRightGhost < lastRight ) {
+            lastRightGhost = lastRight;
+        }
+    }
+
+    double rightTotal = pdp->GetCurrent ( 13 ) + pdp->GetCurrent ( 14 ) + pdp->GetCurrent ( 15 );
+
+    if ( rightTotal > 100 ) {
+        right /= ( 100/rightTotal );
+    }
+
+    if ( voltage < 10 ) {
+        right /= ( 10/voltage );
+    }
+
+    RightVictor->Set ( right );
+
+    lastRight = right;
 }
 
 void Drivetrain::Shift ( bool high )
@@ -38,6 +163,27 @@ void Drivetrain::Shift ( bool high )
                         frc::DoubleSolenoid::Value::kReverse );
     RightSolenoid->Set ( high ? frc::DoubleSolenoid::Value::kForward :
                          frc::DoubleSolenoid::Value::kReverse );
+}
+
+void Drivetrain::EnablePID()
+{
+    LeftPID->Enable();
+    RightPID->Enable();
+
+    LeftEncoder->Reset();
+    RightEncoder->Reset();
+}
+
+void Drivetrain::DisablePID()
+{
+    LeftPID->Disable();
+    RightPID->Disable();
+
+    LeftEncoder->Reset();
+    RightEncoder->Reset();
+
+    lastLeft = 0;
+    lastRight = 0;
 }
 
 void Drivetrain::SetVoltage ( DrivetrainVoltage v )
@@ -70,12 +216,17 @@ double Drivetrain::GetRightVoltage()
     return GetOutputVoltage().right;
 }
 
-double Drivetrain::GetLeftVoltage(void* instance)
+double Drivetrain::GetLeftVoltage ( void* instance )
 {
     return ( ( Drivetrain* ) instance )->GetLeftVoltage();
 }
 
-double Drivetrain::GetRightVoltage(void* instance)
+double Drivetrain::GetRightVoltage ( void* instance )
 {
     return ( ( Drivetrain* ) instance )->GetRightVoltage();
+}
+
+double Drivetrain::GetEncoderValue ( void* instance )
+{
+    return ( ( Encoder* ) instance )->Get();
 }
--- src/Subsystems/Drivetrain.h
+++ src/Subsystems/Drivetrain.h
@@ -2,7 +2,10 @@
 #define DRIVETRAIN_H
 
 #include <Commands/Subsystem.h>
+#include <math.h>
 #include <DoubleSolenoid.h>
+#include <Encoder.h>
+#include <PIDController.h>
 #include "CardinalDash/VictorSP.h"
 
 #include "WPILib.h"
@@ -13,13 +16,21 @@ class Drivetrain : public Subsystem
         Drivetrain();
         void InitDefaultCommand();
         void Set ( double left, double right );
+        void SetRaw ( double left, double right );
+        void SetAdd ( double left, double right );
+        void SetLeftRaw ( double left );
+        void SetRightRaw ( double right );
         void Shift ( bool high );
+
+        void EnablePID();
+        void DisablePID();
 
         struct DrivetrainVoltage {
             double left;
             double right;
 
-            DrivetrainVoltage ( double l, double r ) {
+            DrivetrainVoltage ( double l, double r )
+            {
                 left = l;
                 right = r;
             }
@@ -30,13 +41,35 @@ class Drivetrain : public Subsystem
         DrivetrainVoltage GetOutputVoltage();
         double GetLeftVoltage();
         double GetRightVoltage();
-        static double GetLeftVoltage(void* instance);
-        static double GetRightVoltage(void* instance);
+
     private:
         // Voltage max of battery, if it was 12 then all motors would drive
         // slower when actual voltage > 12. This is because of the division step
         // in SetVoltage which can lead to slower than 100% motor setpoints
         const int VOLTAGE_MAX = 13;
+
+        const double ENCODER_PULSE_PER_ROTATION = 360;
+        const double ENCODER_WHEEL_DIAMETER = 4; // inches
+        const double ENCODER_WHEEL_CIRCUMFERENCE = ENCODER_WHEEL_DIAMETER * M_PI;
+        const double ENCODER_DISTANCE_PER_PULSE = ENCODER_WHEEL_CIRCUMFERENCE / ENCODER_PULSE_PER_ROTATION;
+
+        const double PID_LEFT_P = 0.15;
+        const double PID_LEFT_I = 0.0;
+        const double PID_LEFT_D = 0.01;
+        const double PID_LEFT_F = 0.05;
+
+        const double PID_RIGHT_P = 0.15;
+        const double PID_RIGHT_I = 0.0;
+        const double PID_RIGHT_D = 0.01;
+        const double PID_RIGHT_F = 0.05;
+
+        double lastLeft = 0;
+        double lastLeftGhost = 0;
+        double lastRight = 0;
+        double lastRightGhost = 0;
+
+        const double MAX_ACCEL = 0.005 / 2;
+        const double MAX_GHOST_ACCEL = MAX_ACCEL * 15;
 
         static std::unique_ptr<PowerDistributionPanel> pdp;
 
@@ -46,7 +79,18 @@ class Drivetrain : public Subsystem
         std::unique_ptr<frc::DoubleSolenoid> LeftSolenoid;
         std::unique_ptr<frc::DoubleSolenoid> RightSolenoid;
 
+        std::unique_ptr<frc::Encoder> LeftEncoder;
+        std::unique_ptr<frc::Encoder> RightEncoder;
+
+        std::unique_ptr<frc::PIDController> LeftPID;
+        std::unique_ptr<frc::PIDController> RightPID;
+
         DrivetrainVoltage voltage = DrivetrainVoltage ( 0, 0 );
+
+        static double GetLeftVoltage ( void* instance );
+        static double GetRightVoltage ( void* instance );
+        static double GetEncoderValue ( void* instance );
+
 };
 
 #endif  // DRIVETRAIN_H
